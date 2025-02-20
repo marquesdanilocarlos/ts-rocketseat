ece4b6836f528a56d9d8b4cc7a9afa944acb6215
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-20 20:28:00 -0300

    Aula 08 - Tipagem de array

---
+++ 3-tipos/index.ts
@@ -0,0 +1,11 @@
+let numbers: number[] = [1,2,3]
+numbers.push(5)
+
+console.log(numbers[2])
+
+//Não permite
+//numbers = 'teste'
+//numbers.push('teste')
+
+const names: string[] = ['Danilo', 'Teste'];
+console.log(names)

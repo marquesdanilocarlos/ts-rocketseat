a72269c1b0dc104532a2bba65c4965cd5db6071c
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-19 22:33:24 -0300

    Aula 02 - Tipo Number

---
+++ 2-fundamentos/index.ts
@@ -0,0 +1,10 @@
+//Declaração da variável com tipo number
+let x: number = 10
+
+//Não permite atribuir string
+//x = 'teste'
+
+let y: number = 17.36
+
+console.log(typeof x)
+console.log(typeof y.toPrecision(3))

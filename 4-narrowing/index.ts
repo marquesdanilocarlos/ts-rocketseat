c6a121cc3b98274dee3c90eb4aa9de87c5bb15ed
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-23 12:32:44 -0300

    Aula 26 - Narrowing por instance of

--- 4-narrowing/index.ts
+++ 4-narrowing/index.ts
@@ -28,3 +28,40 @@ function operations(arr: number[], operation?: string | undefined): void {
 }
 
 operations([6,37,7,5,21], '+');
+
+//Instance of
+
+class User {
+    name;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+class SuperUser extends User {
+    constructor(name: string) {
+        super(name);
+    }
+}
+
+const john = new User("John");
+const doe = new SuperUser("Doe");
+
+console.log(john);
+console.log(doe);
+
+function userGreeting(user: object) {
+    if (user instanceof SuperUser) {
+        console.log(`Olá Sr. ${user.name}`);
+        return;
+    }
+
+    if (user instanceof User) {
+        console.log(`Olá ${user.name}`);
+        return;
+    }
+}
+
+userGreeting(john);
+userGreeting(doe);

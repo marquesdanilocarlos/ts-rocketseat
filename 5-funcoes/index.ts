1ff19be55a28e82b2a116e21a907868c003720ff
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-23 13:01:19 -0300

    Aula 29 - Tipagem de callback como argumento

--- 5-funcoes/index.ts
+++ 5-funcoes/index.ts
@@ -6,3 +6,18 @@ function withoutReturn() : void {
 }
 
 withoutReturn();
+
+//Callback como argumento
+
+function greeting(name: string) : string {
+    return `Olá ${name}!`;
+}
+
+function preGreeting(name: string, fn: (name: string) => string) : void {
+    console.log(`Preparando a função`);
+
+    const greeting = fn(name);
+    console.log(greeting);
+}
+
+preGreeting('Danilo', greeting);

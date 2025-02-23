d16630579c9aa2f9f864208ccbec789027613039
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-23 12:27:09 -0300

    Aula 25 - Narrowing pela checagem da existência de valor

--- 4-narrowing/index.ts
+++ 4-narrowing/index.ts
@@ -1,5 +1,4 @@
 //Type-guard
-
 function sum(a: number | string, b: number | string): void {
     if (typeof a == "string" && typeof b == "string") {
         console.log(parseFloat(a) + parseFloat(b));
@@ -7,12 +6,25 @@ function sum(a: number | string, b: number | string): void {
     }
 
     if (typeof a == "number" && typeof b == "number") {
-        console.log(a+b);
+        console.log(a + b);
         return;
     }
 
     console.log("Impossível realizar a soma");
 }
 
-sum(16,96);
-sum("16","96");
+sum(16, 96);
+sum("16", "96");
+
+//Check de existência do valor
+
+function operations(arr: number[], operation?: string | undefined): void {
+    if (!operation) {
+        console.log("Defina a operação");
+        return;
+    }
+
+    console.log("Vai realizar a operação");
+}
+
+operations([6,37,7,5,21], '+');

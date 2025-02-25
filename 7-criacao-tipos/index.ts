cfb32bde7fd6466be1f413d63514438942de8019
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-24 23:03:42 -0300

    Aula 45 - Generics com constraints

--- 7-criacao-tipos/index.ts
+++ 7-criacao-tipos/index.ts
@@ -1,6 +1,23 @@
+/**
+ * Generics
+ */
 function showData<T>(arg: T): string {
     return `O dado é ${arg}`;
 }
 
 console.log(showData(5));
 console.log(showData('Olá'));
+
+/**
+ * Constraints em generics
+ */
+
+function showProductName<T extends { name: string, price: number }>(obj: T): string {
+    return `O produto é ${obj.name}`;
+}
+
+const myObj = { name: 'Notebook', price: 2500 };
+const otherProduct = { name: 'Geladeira'};
+console.log(showProductName(myObj));
+//Não permite
+//console.log(showProductName(otherProduct));

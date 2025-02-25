4038657463ca1fd27d83021d41faec98d522ec4d
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-24 23:08:47 -0300

    Aula 46 - Interfaces com Generics

--- 7-criacao-tipos/index.ts
+++ 7-criacao-tipos/index.ts
@@ -16,8 +16,28 @@ function showProductName<T extends { name: string, price: number }>(obj: T): str
     return `O produto é ${obj.name}`;
 }
 
-const myObj = { name: 'Notebook', price: 2500 };
-const otherProduct = { name: 'Geladeira'};
+const myObj = {name: 'Notebook', price: 2500};
+const otherProduct = {name: 'Geladeira'};
 console.log(showProductName(myObj));
 //Não permite
 //console.log(showProductName(otherProduct));
+
+/**
+ * Generics com interface
+ */
+
+interface AbstractObj<T, U, Q> {
+    name: string;
+    wheels: T;
+    engine: U;
+    color:Q;
+}
+
+type Car = AbstractObj<number, number, boolean>;
+type Pen = AbstractObj<boolean, boolean, string>;
+
+const myCar:Car = {name: 'Fusca', wheels: 4, engine: 1.0, color: false};
+const myPen:Pen = {name: 'Bic', wheels: false, engine: false, color: 'azul'};
+
+console.log(myCar);
+console.log(myPen);

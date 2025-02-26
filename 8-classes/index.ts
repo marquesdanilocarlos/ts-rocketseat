89fefc150185b3547ff0c454cb6f840d54f9d823
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-25 21:25:55 -0300

    Aula 58 - This

--- 8-classes/index.ts
+++ 8-classes/index.ts
@@ -64,3 +64,26 @@ console.log(tractor);
 console.log(destroyer);
 
 destroyer.kill();
+
+/**
+ * This
+ */
+
+class Truck {
+    model: string;
+    hp: number;
+
+    constructor(model:string, hp:number) {
+        this.model = model;
+        this.hp = hp;
+    }
+
+    showDetails(): void {
+        console.log(`Caminhão do modelo: ${this.model}, que tem ${this.hp} cavalos de potência`);
+    }
+}
+
+const scania = new Truck('Scania', 500);
+const volvo = new Truck('Volvo', 450);
+scania.showDetails();
+volvo.showDetails();

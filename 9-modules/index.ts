0853c4d6f69347c16b3447abb270066bce66a9bb
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-25 23:15:52 -0300

    Aula 74 - Import Interface

--- 9-modules/index.ts
+++ 9-modules/index.ts
@@ -33,3 +33,24 @@ import * as numbers from "./numbers";
 console.log(numbers.n1);
 console.log(numbers.n2);
 console.log(numbers.sumNumbers());
+
+/**
+ * Import interface
+ */
+
+import {Human} from "./myType";
+
+class User implements Human {
+    name: string;
+    age: number;
+    phone: string;
+
+    constructor(name: string, age: number, phone: string) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+}
+
+const danilo = new User('Danilo', 25, '9999-9999');
+console.log(danilo);

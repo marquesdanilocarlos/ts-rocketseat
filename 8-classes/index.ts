4f362e954b487bf5d108ffacccc91c207d6070af
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-25 21:14:22 -0300

    Aula 54 - Método construtor

--- 8-classes/index.ts
+++ 8-classes/index.ts
@@ -1,11 +1,15 @@
 class User {
     name: string;
     age: number;
+    constructor(name:string, age:number) {
+        this.name = name;
+        this.age = age;
+    }
 }
 
-const danilo = new User();
-danilo.name = 'Danilo';
-danilo.age = 33;
+const danilo = new User('Danilo', 33);
+//danilo.name = 'Danilo';
+//danilo.age = 33;
 
 //Não permite
 //danilo.job = 'programmer';

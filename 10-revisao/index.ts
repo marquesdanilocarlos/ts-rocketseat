2a8c60ac6e5b87676cd290ab84525a083f1161b0
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-26 20:35:20 -0300

    Aula 75 - Revisão de tipos primitivos

---
+++ 10-revisao/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Tipo primitivos
+ */
+
+let username: string;
+
+username = 'Danilo';
+//Não permite
+//username = 10;
+console.log(username);
+
+let age: number;
+//Não permite
+//age = '33';
+age = 33;
+age = 33.5;
+console.log(age);
+
+let isLoading: boolean;
+isLoading = true;
+console.log(isLoading);
+
+

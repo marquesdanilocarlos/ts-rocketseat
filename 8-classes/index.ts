74f54c41eed4cf550b676064a7357c5dc532a3ae
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-25 21:11:50 -0300

    Aula 53 - Classes

---
+++ 8-classes/index.ts
@@ -0,0 +1,14 @@
+class User {
+    name: string;
+    age: number;
+}
+
+const danilo = new User();
+danilo.name = 'Danilo';
+danilo.age = 33;
+
+//Não permite
+//danilo.job = 'programmer';
+
+
+console.log(danilo);

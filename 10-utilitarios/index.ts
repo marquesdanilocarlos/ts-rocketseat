e907db8e93dd845d1ba296dc3ada564446dc97be
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2025-02-27 11:04:16 -0300

    Aula 91 - Omit

--- 10-utilitarios/index.ts
+++ 10-utilitarios/index.ts
@@ -21,9 +21,16 @@ interface Book {
     title: string,
     pages: number,
     author: string,
+    description: string
 }
 
 const book1: Pick<Book, 'title'> = {title: 'Livro 1'};
 console.log(book1);
 const book2: Pick<Book, 'title' | 'pages'> = {title: 'Livro 2', pages: 532};
 console.log(book2);
+
+/**
+ * Omit
+ */
+const book: Omit<Book, 'description'> = {title: 'Livro 3', pages: 300, author: 'Danilo'};
+console.log(book);
